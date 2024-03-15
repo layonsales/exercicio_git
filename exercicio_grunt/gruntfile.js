e097426aaf5e3c29699920d30e8b567660291ca1
layon <layonsilvaf5@gmail.com>
2024-03-15 18:02:38 -0300

    done

---
+++ exercicio_grunt/gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                files: {
+                    'dist/styles/main.css': 'source/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'source/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+
+    grunt.registerTask('default', ['less', 'uglify'])
+}
